af6d61938e5c45a558d8206037238a8dfe887e4f
ajsdkty3 <98349232+ajsdkty3@users.noreply.github.com>
2026-03-01 03:26:48 -0600

    add some sounds

--- Assets/Gameplay/Scripts/Core/CameraShake2D.cs
+++ Assets/Gameplay/Scripts/Core/CameraShake2D.cs
@@ -1,16 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraShake2D : MonoBehaviour {
     public static CameraShake2D I { get; private set; }
 
     Vector3 _baseLocalPos;
+
     float _shakeTime;
+    float _shakeDuration;
     float _shakeStrength;
 
     void Awake() {
-        if (I != null && I != this) { Destroy(gameObject); return; }
-        I = this;
+        if (I != null && I != this) {
+            Destroy(gameObject);
+            return;
+        }
 
+        I = this;
+    }
+
+    void OnEnable() {
         _baseLocalPos = transform.localPosition;
     }
 
@@ -18,7 +26,10 @@ public class CameraShake2D : MonoBehaviour {
         if (_shakeTime > 0f) {
             _shakeTime -= Time.unscaledDeltaTime;
 
-            Vector2 offset = Random.insideUnitCircle * _shakeStrength;
+            float normalized = _shakeTime / _shakeDuration; // 逐渐减弱
+            float currentStrength = _shakeStrength * normalized;
+
+            Vector2 offset = Random.insideUnitCircle * currentStrength;
             transform.localPosition = _baseLocalPos + (Vector3)offset;
         } else {
             transform.localPosition = _baseLocalPos;
@@ -26,7 +37,12 @@ public class CameraShake2D : MonoBehaviour {
     }
 
     public void Shake(float duration, float strength) {
-        _shakeTime = Mathf.Max(_shakeTime, duration);
+        if (duration <= 0f)
+            return;
+
+        _shakeDuration = duration;
+        _shakeTime = duration;
+
         _shakeStrength = Mathf.Max(_shakeStrength, strength);
     }
 }

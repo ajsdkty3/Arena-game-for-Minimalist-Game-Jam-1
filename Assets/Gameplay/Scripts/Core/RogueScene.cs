06372a2a93ed0fb321b0e3031743b3a6a76914be
Roguelike <minhaoxxxt@gmail.com>
2026-03-03 23:44:12 +0300

    Almost finished

---
+++ Assets/Gameplay/Scripts/Core/RogueScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RogueScene : MonoBehaviour
+{
+    public void PlayRogue()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
